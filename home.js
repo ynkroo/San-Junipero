17c1b19d71206aa3544905c5e653016aa8a60db6
Yannick Röösli <yannick.roeoesli@stud.hslu.ch>
2024-05-05 11:26:00 +0200

    gradient

--- home.js
+++ home.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   var angle = 0; // Initial angle
-  var element = document.querySelector(".CircleHome"); // Select the element
+  var element = document.querySelector(".GlassCircle"); // Select the element
 
   element.addEventListener("click", function () {
     angle += 120; // Increase angle by 90 degrees on each click
-    element.style.transform = "rotate(" + angle + "deg)"; // Apply the rotation
+    element.style.transform = "translate(-50%, -50%) rotate(" + angle + "deg)";
   });
 });
 
@@ -18,3 +18,20 @@ function spaNavigate(data) {
   // With a transition:
   document.startViewTransition(() => updateTheDOMSomehow(data));
 }
+
+document.addEventListener("visibilitychange", function () {
+  if (document.visibilityState === "visible") {
+    const heroImage = document.querySelector(".HeroImage");
+
+    // Force reflow/repaint
+    heroImage.style.animation = "none";
+    void heroImage.offsetWidth; // This line is crucial, as it forces a reflow
+
+    // Re-apply the animation
+    heroImage.style.animation = "";
+    setTimeout(() => {
+      heroImage.style.animation =
+        "fadeinLoad 10s ease 1s, fadeUp 5s ease-out 0.5s";
+    }, 50); // A small delay to ensure the styles are applied
+  }
+});
