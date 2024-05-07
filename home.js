f35404efcd61060d9d68c55aa0846687b5d4e329
Yannick Röösli <yannick.roeoesli@stud.hslu.ch>
2024-05-07 16:52:40 +0200

    adding music, small changes

--- home.js
+++ home.js
@@ -19,3 +19,22 @@ document.addEventListener("DOMContentLoaded", function () {
     dragMeImage.style.top = e.pageY - 32 + "px"; // Assuming the image is 32px high
   });
 });
+
+// Get a reference to the audio element
+const audioElement = document.getElementById("myAudio");
+
+// Listen for keydown events
+document.addEventListener("keydown", function (event) {
+  // Check if the pressed key is the spacebar (key code 32)
+  if (event.code === "Space") {
+    // Prevent default spacebar behavior (scrolling)
+    event.preventDefault();
+
+    // Toggle between play and pause
+    if (audioElement.paused) {
+      audioElement.play();
+    } else {
+      audioElement.pause();
+    }
+  }
+});
